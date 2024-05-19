ba3f5ad053e8606bfe68124682e4a955ef5ba524
thephotogenicbug <navinkumarachari@gmail.com>
2024-05-19 13:03:55 +0530

    Projects

---
+++ 07_projects/Keyboard/index.js
@@ -0,0 +1,22 @@
+const insert = document.getElementById("insert");
+
+window.addEventListener("keydown", function (event) {
+  insert.innerHTML = `
+      <div class='color'>
+       <table>
+        <tr>
+            <th>Key</th>
+            <th>Keycode</th>
+            <th>Code</th>
+        </tr>
+        <tr>
+            <td>${event.key === null ? "Space" : event.key}</td>
+            <td>${event.keyCode}</td>
+            <td>${event.code}</td>
+        </tr>
+      
+       </table>
+      </div>
+
+    `;
+});
